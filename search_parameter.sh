9cbd166927795d6aa5835fecdd22b2f029b89070
Adam Novak <anovak@soe.ucsc.edu>
2024-01-05 09:08:34 -0800

    Get param search script working

--- search_parameter.sh
+++ search_parameter.sh
@@ -14,35 +14,45 @@ set -ex
 
 : "${OPTION_NAME:=downsample-min}"
 : "${OPTION_RANGE:=30:200:20}"
+: "${PRESET:=sr}"
 
 
-"${SEARCH_DIR:=${WORK_DIR}/${OPTION_NAME}}"
+SEARCH_DIR="${WORK_DIR}/${OPTION_NAME}"
 
 mkdir -p "${SEARCH_DIR}"
 
-srun -c16 --mem 300G --job-name giraffe-run vg giraffe --parameter-preset sr --progress --track-provenance -Z ${GRAPH_BASE}.gbz -d ${GRAPH_BASE}.dist -m ${GRAPH_BASE}.${MINPARAMS}.withzip.min -z ${GRAPH_BASE}.${MINPARAMS}.zipcodes -G ${INPUT_READS} --${OPTION_NAME} ${OPTION_RANGE} --output-basename ${SEARCH_DIR}/search
+srun -c20 --time 01:00:00 --partition=medium --mem 300G --job-name giraffe-run vg giraffe -t16 --parameter-preset ${PRESET} --progress --track-provenance -Z ${GRAPH_BASE}.gbz -d ${GRAPH_BASE}.dist -m ${GRAPH_BASE}.${MINPARAMS}.withzip.min -z ${GRAPH_BASE}.${MINPARAMS}.zipcodes -G ${INPUT_READS} --${OPTION_NAME} ${OPTION_RANGE} --output-basename ${SEARCH_DIR}/search
 
+SLURM_JOBS=()
 for GAM_FILE in ${SEARCH_DIR}/*.gam ; do
     if [[ ! -e "${GAM_FILE%.gam}.compare.txt" ]] ; then
-        sbatch -c4 --mem 25G --job-name giraffe-eval --wrap "vg annotate -a ${GAM_FILE} -x ${GRAPH_BASE}.gbz -m | vg gamcompare --range 200 - ${INPUT_READS} -T -a "${GAM_FILE}" > ${GAM_FILE%.gam}.compared.tsv 2>${GAM_FILE%.gam}.compare.txt"
+        SLURM_JOBS+=($(sbatch --parsable -c4 --time 01:00:00 --partition=medium --mem 25G --job-name giraffe-eval --wrap "vg annotate -a ${GAM_FILE} -x ${GRAPH_BASE}.gbz -m | vg gamcompare --range 200 - ${INPUT_READS} -T -a "${GAM_FILE}" > ${GAM_FILE%.gam}.compared.tsv 2>${GAM_FILE%.gam}.compare.txt"))
     fi
     if [[ ! -e "${GAM_FILE%.gam}.gamstats.txt" ]] ; then
-        sbatch -c2 --mem 10G --job-name giraffe-stats --wrap "vg stats -a "${GAM_FILE}" >${GAM_FILE%.gam}.gamstats.txt"
+        SLURM_JOBS+=($(sbatch --parsable -c2 --time 01:00:00 --partition=medium --mem 10G --job-name giraffe-stats --wrap "vg stats -a "${GAM_FILE}" >${GAM_FILE%.gam}.gamstats.txt"))
     fi
 done
 
-./swait
+# If there are any jobs in the array, wait for them to finish
+if [[ "${#SLURM_JOBS[@]}" != "0" ]] ; then
+    # Wait for these particular jobs to finish 
+    QUEUE_LINES=0
+    while [[ "${QUEUE_LINES}" != "1" ]] ; do
+        sleep 2
+        QUEUE_LINES="$(squeue -u $USER -j $(IFS=,; echo "${SLURM_JOBS[*]}") | wc -l)"
+    done
+fi
 
 COMPARISON_SCRATCH="${WORK_DIR}/combined.tsv"
 printf "correct\tmq\taligner\tread\teligible\n" >"${COMPARISON_SCRATCH}"
 cat "${SEARCH_DIR}"/*.compared.tsv | sed "s_${SEARCH_DIR}[-/a-zA-Z]*_v_g" | sed 's_.gam__g' | grep -v "^correct" >>"${COMPARISON_SCRATCH}"
 
 # Plot PR and QQ
-srun -c2 --mem 10G Rscript scripts/plot-pr.R "${COMPARISON_SCRATCH}" ${WORK_DIR}/${OPTION_NAME}.compared.png
-srun -c2 --mem 10G Rscript scripts/plot-qq.R "${COMPARISON_SCRATCH}" ${WORK_DIR}/${OPTION_NAME}.qq.png
+srun -c2 --mem 10G Rscript ./plot-pr.R "${COMPARISON_SCRATCH}" ${WORK_DIR}/${OPTION_NAME}.compared.png
+srun -c2 --mem 10G Rscript ./plot-qq.R "${COMPARISON_SCRATCH}" ${WORK_DIR}/${OPTION_NAME}.qq.png
 
 # Dump QC stats and speed
-rm ${WORK_DIR}/${OPTION_NAME}.stats.txt
+rm -f ${WORK_DIR}/${OPTION_NAME}.stats.txt
 for GAM_FILE in ${SEARCH_DIR}/*.gam ; do
     echo "${GAM_FILE}" >>${WORK_DIR}/${OPTION_NAME}.stats.txt
     cat ${GAM_FILE%.gam}.compare.txt >>${WORK_DIR}/${OPTION_NAME}.stats.txt
