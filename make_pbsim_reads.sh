07d712b7f561e99b130c92d89bd24e4fa8d12c9d
Adam Novak <anovak@soe.ucsc.edu>
2024-08-06 14:56:35 -0700

    Teach read simulator to tag reads with overlapped features from e.g. centromere BEDs

--- make_pbsim_reads.sh
+++ make_pbsim_reads.sh
@@ -27,6 +27,27 @@ set -ex
 : "${SAMPLE_FASTQ:=/private/groups/patenlab/anovak/projects/hprc/lr-giraffe/reads/real/hifi/HiFi_reads_100k.fq}"
 # HMM model to use instead of a FASTQ, or "/dev/null"
 : "${PBSIM_HMM:=/dev/null}"
+# BED or bigBed URL or file to use to annotate reads with overlapped regions.
+: "${REGION_TAG_BED_URL:=""}"
+# Name of a contig (e.g. chrY) to pull from a different region tag BED 
+: "${REPLACE_REGION_TAG_BED_CONTIG:=""}"
+# BED or bigBED URL or file to use for that contig specifically.
+: "${REPLACE_REGION_TAG_BED_URL:=""}"
+# chromAliasBb style alias file mapping region tag BED contigs to canonical ones in haplotype 1
+: "${HAPLOTYPE_1_ALIAS_URL:=""}"
+# Column in the alias BED for haplotype 1 to apply
+: "${HAPLOTYPE_1_ALIAS_COLUMN:=6}"
+# Prefix to apply to haplotype 1 canonical contigs to get graph path names
+: "${HAPLOTYPE_1_ALIAS_PREFIX:=}"
+# chromAliasBb style alias file mapping region tag BED contigs to canonical ones in haplotype 2
+: "${HAPLOTYPE_2_ALIAS_URL:=""}"
+# Column in the alias BED for haplotype 2 to apply
+: "${HAPLOTYPE_2_ALIAS_COLUMN:=6}"
+# Prefix to apply to haplotype 2 canonical contigs to get graph path names
+: "${HAPLOTYPE_2_ALIAS_PREFIX:=""}"
+# Prefix to apply to all region tag BED contigs to get graph path names
+: "${REGION_TAG_BED_PREFIX:=""}"
+
 # This needs to be the pbsim2 binary, which might not be in $PATH.
 # It can be installed with
 # git clone https://github.com/yukiteruono/pbsim2.git
@@ -52,6 +73,49 @@ set -ex
 # Group to make output directory writable to
 : "${OUT_DIR_GROUP=""}"
 
+# Function to fetch a URL or path to a local path, if the local path doesn't
+# already exist. Won't create the destination until the download succeeds.
+function fetch() {
+    local URL="${1}"
+    local DEST_PATH="${2}"
+
+    if [[ ! -e "${DEST_PATH}" ]] ; then
+        # This comparison require Bash 3 or later. See <https://stackoverflow.com/a/2172365>
+        if [[ ${URL} =~ ^s3:.* ]]; then
+            # Download from S3
+            aws s3 cp "${URL}" "${DEST_PATH}.tmp"
+            mv "${DEST_PATH}.tmp" "${DEST_PATH}"
+        elif [[ ${URL} =~ ^http:.* || ${URL} =~ ^https:.* ]]; then
+            # Download from the web
+            wget "${URL}" -O "${DEST_PATH}.tmp"
+            mv "${DEST_PATH}.tmp" "${DEST_PATH}"
+        else
+            # Use local symlink
+            ln -s "$(realpath "${URL}")" "${DEST_PATH}"
+        fi
+    fi
+}
+
+# Function to fetch a BED or bigBed from a URL or file path, and put a BED at
+# the given destination path.
+function fetchBed() {
+    local URL="${1}"
+    local DEST_PATH="${2}"
+    
+    if [[ ! -e "${DEST_PATH}" ]] ; then
+        if [[ ${URL} =~ .*\.bb ]] ; then
+            # Looks like a bigBed so fetch that and convert
+            fetch "${URL}" "${DEST_PATH}.bb"
+            bigBedToBed "${DEST_PATH}.bb" "${DEST_PATH}.tmp"
+            mv "${DEST_PATH}.tmp" "${DEST_PATH}"
+        else
+            # Looks like a normal BED
+            fetch "${URL}" "${DEST_PATH}"
+        fi
+    fi
+}
+
+
 if [[ "${WORK_DIR}" == "" ]] ; then
     # Make a work directory
     WORK_DIR="$(mktemp -d)"
@@ -61,34 +125,101 @@ else
     CLEAN_WORK_DIR=0
 fi
 
-
 # Make sure scratch directory exists
 mkdir -p "${WORK_DIR}"
+
+
+if [[ ! -z "${REGION_TAG_BED_URL}" ]] ; then
+    # We need to prepare a BED for this sample of regions to tag
+    TAG_BED="${WORK_DIR}/${SAMPLE_NAME}.regions.bed" 
+    if [[ ! -e "${TAG_BED}" ]] ; then
+        # We need to fetch and splice BEDs
+        # For this we need a separate scratch dir
+        # TODO: Stop using the check and .tmp and move trick since nobody else can see in here!
+        BED_SCRATCH=$(mktemp -d)
+        
+        fetchBed "${REGION_TAG_BED_URL}" "${BED_SCRATCH}/${SAMPLE_NAME}.original.bed"
+        
+        if [[ ! -z "${REPLACE_REGION_TAG_BED_CONTIG}" ]] ; then
+            # If we need to replace a contig
+
+            # Get the replace BED
+            fetchBed "${REPLACE_REGION_TAG_BED_URL}" "${BED_SCRATCH}/${SAMPLE_NAME}.replace.bed"
+            
+            if [[ ! -e "${BED_SCRATCH}/${SAMPLE_NAME}.intermediate.bed" ]] ; then
+                # Take the other contigs from the original bed, and the
+                # replacement contig from the replace BED, and paste them
+                # together into the intermediate BED
+
+                (grep -P -v "^${REPLACE_REGION_TAG_BED_CONTIG}\t" "${BED_SCRATCH}/${SAMPLE_NAME}.original.bed"; grep -P "^${REPLACE_REGION_TAG_BED_CONTIG}\t" "${BED_SCRATCH}/${SAMPLE_NAME}.replace.bed") >"${BED_SCRATCH}/${SAMPLE_NAME}.intermediate.bed.tmp"
+                mv "${BED_SCRATCH}/${SAMPLE_NAME}.intermediate.bed.tmp" "${BED_SCRATCH}/${SAMPLE_NAME}.intermediate.bed"
+            fi
+
+        else
+            # Else pass the original one through as the intermediate BED
+            if [[ ! -e "${BED_SCRATCH}/${SAMPLE_NAME}.intermediate.bed" ]] ; then
+                cp "${BED_SCRATCH}/${SAMPLE_NAME}.original.bed" "${BED_SCRATCH}/${SAMPLE_NAME}.intermediate.bed.tmp"
+                mv "${BED_SCRATCH}/${SAMPLE_NAME}.intermediate.bed.tmp" "${BED_SCRATCH}/${SAMPLE_NAME}.intermediate.bed"
+            fi
+        fi
+        
+        if [[ ! -z "${HAPLOTYPE_1_ALIAS_URL}" ]] ; then
+            # Then get the aliases for haplotype 1 and make a sed script
+            
+            fetchBed "${HAPLOTYPE_1_ALIAS_URL}" "${BED_SCRATCH}/${SAMPLE_NAME}.alias1.bed"
+
+            if [[ ! -e "${BED_SCRATCH}/${SAMPLE_NAME}.alias1.sed" ]] ; then
+                cat "${BED_SCRATCH}/${SAMPLE_NAME}.alias1.bed"  | cut -f1,${HAPLOTYPE_1_ALIAS_COLUMN} | sed 's/\(.*\)\t\(.*\)/s!\2!'"${HAPLOTYPE_1_ALIAS_PREFIX}"'\1!g/g' >"${BED_SCRATCH}/${SAMPLE_NAME}.alias1.sed.tmp"
+                mv "${BED_SCRATCH}/${SAMPLE_NAME}.alias1.sed.tmp" "${BED_SCRATCH}/${SAMPLE_NAME}.alias1.sed" 
+            fi
+
+        fi
+
+        if [[ ! -z "${HAPLOTYPE_2_ALIAS_URL}" ]] ; then
+            # Also get the aliases for haplotype 2 and make a sed script
+            
+            fetchBed "${HAPLOTYPE_2_ALIAS_URL}" "${BED_SCRATCH}/${SAMPLE_NAME}.alias2.bed"
+
+            if [[ ! -e "${BED_SCRATCH}/${SAMPLE_NAME}.alias2.sed" ]] ; then
+                cat "${BED_SCRATCH}/${SAMPLE_NAME}.alias2.bed"  | cut -f1,${HAPLOTYPE_2_ALIAS_COLUMN} | sed 's/\(.*\)\t\(.*\)/s!\2!'"${HAPLOTYPE_2_ALIAS_PREFIX}"'\1!g/g' >"${BED_SCRATCH}/${SAMPLE_NAME}.alias2.sed.tmp"
+                mv "${BED_SCRATCH}/${SAMPLE_NAME}.alias2.sed.tmp" "${BED_SCRATCH}/${SAMPLE_NAME}.alias2.sed" 
+            fi
+
+        fi
+        
+        if [[ ! -z "${REGION_TAG_BED_PREFIX}" ]] ; then
+            # And make a script to apply the overall prefix (with a name later than the alias scripts)
+            echo 's/^/'"${REGION_TAG_BED_PREFIX}"'/g' >"${BED_SCRATCH}/${SAMPLE_NAME}.prefix.sed.tmp"
+            mv "${BED_SCRATCH}/${SAMPLE_NAME}.prefix.sed.tmp" "${BED_SCRATCH}/${SAMPLE_NAME}.prefix.sed" 
+        fi
+
+        if [[ ! -e "${BED_SCRATCH}/${SAMPLE_NAME}.combined-sed" ]] ; then
+            # Combine the sed scripts. Turn on nullglob in case there aren't any.
+            shopt -s nullglob
+            cat "${BED_SCRATCH}/${SAMPLE_NAME}."*".sed" >"${BED_SCRATCH}/${SAMPLE_NAME}.combined-sed.tmp"
+            shopt -u nullglob
+            mv "${BED_SCRATCH}/${SAMPLE_NAME}.combined-sed.tmp" "${BED_SCRATCH}/${SAMPLE_NAME}.combined-sed"
+        fi
+        
+        # And apply the sed script to create the final tag BED
+        sed -f "${BED_SCRATCH}/${SAMPLE_NAME}.combined-sed" "${BED_SCRATCH}/${SAMPLE_NAME}.intermediate.bed" >"${TAG_BED}.tmp"
+
+        # And put it into place
+        mv "${TAG_BED}.tmp" "${TAG_BED}"
+        
+        # And clean up the scratch
+        rm -Rf "${BED_SCRATCH}"
+    fi
+else
+    # No need to tag anything
+    TAG_BED=""
+fi
 
 if [[ -z "${GRAPH_GBZ_URL}" ]] ; then
 
     # Fetch graph
-    if [[ ! -e "${WORK_DIR}/${GRAPH_NAME}.xg" ]] ; then
-        # This comparison require Bash 3 or later. See <https://stackoverflow.com/a/2172365>
-        if [[ ${GRAPH_XG_URL} =~ ^s3:.* ]]; then
-            # Download from S3
-            aws s3 cp "${GRAPH_XG_URL}" "${WORK_DIR}/${GRAPH_NAME}.xg.tmp"
-            mv "${WORK_DIR}/${GRAPH_NAME}.xg.tmp" "${WORK_DIR}/${GRAPH_NAME}.xg"
-        else
-            # Use local symlink
-            ln -s "$(realpath "${GRAPH_XG_URL}")" "${WORK_DIR}/${GRAPH_NAME}.xg"
-        fi
-    fi
-    if [[ ! -e "${WORK_DIR}/${GRAPH_NAME}.gbwt" ]] ; then
-        if [[ ${GRAPH_GBWT_URL} =~ ^s3:.* ]]; then
-            # Download from S3
-            aws s3 cp "${GRAPH_GBWT_URL}" "${WORK_DIR}/${GRAPH_NAME}.gbwt.tmp"
-            mv "${WORK_DIR}/${GRAPH_NAME}.gbwt.tmp" "${WORK_DIR}/${GRAPH_NAME}.gbwt"
-        else
-            # Use local symlink
-            ln -s "$(realpath "${GRAPH_GBWT_URL}")" "${WORK_DIR}/${GRAPH_NAME}.gbwt"
-        fi
-    fi
+    fetch "${GRAPH_XG_URL}" "${WORK_DIR}/${GRAPH_NAME}.xg"
+    fetch "${GRAPH_GBWT_URL}" "${WORK_DIR}/${GRAPH_NAME}.gbwt"
 
     if [[ ! -e "${WORK_DIR}/${GRAPH_NAME}.gbz" ]] ; then
         # Make it one file
@@ -98,15 +229,7 @@ if [[ -z "${GRAPH_GBZ_URL}" ]] ; then
 
 elif [[ ! -e "${WORK_DIR}/${GRAPH_NAME}.gbz" ]] ; then
     # Fetch the GBZ
-    if [[ ${GRAPH_GBZ_URL} =~ ^s3:.* ]]; then
-        # Download from S3
-        aws s3 cp "${GRAPH_GBZ_URL}" "${WORK_DIR}/${GRAPH_NAME}.gbz.tmp"
-        mv "${WORK_DIR}/${GRAPH_NAME}.gbz.tmp" "${WORK_DIR}/${GRAPH_NAME}.gbz"
-    else
-        # Use local symlink
-        ln -s "$(realpath "${GRAPH_GBZ_URL}")" "${WORK_DIR}/${GRAPH_NAME}.gbz"
-    fi
-
+    fetch "${GRAPH_GBZ_URL}" "${WORK_DIR}/${GRAPH_NAME}.gbz"
 fi
 
 if [[ ! -e "${WORK_DIR}/${GRAPH_NAME}-${SAMPLE_NAME}-as-ref.gbz" ]] ; then
@@ -246,6 +369,18 @@ if [[ "${TOTAL_READS}" -lt 1000500 ]] ; then
 fi
 echo "Simulated ${TOTAL_READS} reads overall"
 
+if [[ ! -z "${TAG_BED}" ]] ; then
+    # Tag reads with the tag BED
+    if [[ ! -e "" ]] ; then
+        time "${VG}" annotate -t 16 -a "${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}.gam" -x "${WORK_DIR}/${GRAPH_NAME}-${SAMPLE_NAME}-as-ref.gbz" --bed-name "${TAG_BED}" >"${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}.tagged.gam.tmp"
+        mv "${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}.tagged.gam.tmp" "${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}.tagged.gam"
+        GAM_TO_SUBSET="${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}.tagged.gam"
+    fi
+else
+    # Pass through reads without tagging
+    GAM_TO_SUBSET="${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}.gam"
+fi
+
 SUBSAMPLE_SEED=1
 for READ_COUNT in 100 1000 10000 100000 1000000 ; do
     # Subset to manageable sizes (always)
@@ -253,7 +388,7 @@ for READ_COUNT in 100 1000 10000 100000 1000000 ; do
     FRACTION="$(echo "(${READ_COUNT} + 500)/${TOTAL_READS}" | bc -l | sed 's/^[0-9]*//g')"
     
     if [[ ! -e "${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}-${READ_COUNT}.gam" ]] ; then
-        "${VG}" filter -d "${SUBSAMPLE_SEED}${FRACTION}" "${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}.gam" >"${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}.coarse.gam"
+        "${VG}" filter -d "${SUBSAMPLE_SEED}${FRACTION}" "${GAM_TO_SUBSET}" >"${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}.coarse.gam"
         "${VG}" gamsort --shuffle "${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}.coarse.gam" >"${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}.coarse.shuffled.gam"
         "${VG}" filter -t1 --max-reads "${READ_COUNT}" "${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}.coarse.shuffled.gam" >"${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}-${READ_COUNT}.gam.tmp"
         mv "${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}-${READ_COUNT}.gam.tmp" "${SAMPLE_WORK_DIR}/${SAMPLE_NAME_OUT}-sim-${TECH_NAME}-${READ_COUNT}.gam"
