f3b706a4444bb38f3952a532806d9aca2ba0c81d
Xian <xhchang@ucsc.edu>
2025-07-16 22:30:46 +0200

    Make categories a variable but need another color

--- make_paper_figures.sh
+++ make_paper_figures.sh
@@ -9,9 +9,9 @@ OUT_DIR="./plots"
 # Asking for all_paper_figures also works
 #(umask 0002 ; snakemake -p --dry-run --keep-going --rerun-triggers mtime --resources full_cluster_nodes=2 threads=256 --slurm --rerun-incomplete --latency-wait 120 -j128 --use-singularity --singularity-args '-B /private/home/jmonlong/workspace/lreval/data/:/private/home/jmonlong/workspace/lreval/data/ -B /private/groups/patenlab/project-lrg/:/private/groups/patenlab/project-lrg/ -B /private/groups/patenlab/anovak/projects/hprc/lr-giraffe/:/private/groups/patenlab/anovak/projects/hprc/lr-giraffe/' \
 #/private/groups/patenlab/project-lrg/experiments/hifi_sim_1m_headline/results/compared.tsv \
-#/private/groups/patenlab/project-lrg/experiments/r10_sim_1m_headline/results/compared.tsv \
+#/private/groups/patenlab/project-lrg/experiments/r10y2025_sim_1m_headline/results/compared.tsv \
 #/private/groups/patenlab/project-lrg/experiments/hifi_sim_1m_headline/results/mapping_stats_sim.tsv \
-#/private/groups/patenlab/project-lrg/experiments/r10_sim_1m_headline/results/mapping_stats_sim.tsv \
+#/private/groups/patenlab/project-lrg/experiments/r10y2025_sim_1m_headline/results/mapping_stats_sim.tsv \
 #/private/groups/patenlab/project-lrg/experiments/hifi_real_full_headline/results/softclipped_or_unmapped_percent.tsv \
 #/private/groups/patenlab/project-lrg/experiments/r10_real_full_headline/results/softclipped_or_unmapped_percent.tsv \
 #/private/groups/patenlab/project-lrg/experiments/hifi_real_full_headline/results/run_and_sampling_time_from_benchmark.tsv \
@@ -31,15 +31,17 @@ GREEN="'#27A974'"
 YELLOW="'#F2C300'"
 DARK_ORANGE="'#C44601'"
 ORANGE="'#F57600'"
+NEW_COLOR="???"
 
 
 MINIMAP_COLOR=${PURPLE}
 WINNOWMAP_COLOR=${LIGHT_PURPLE}
 GIRAFFE_PRIMARY_COLOR=${BLUE}
 PBMM_COLOR=${LIGHT_BLUE}
-MINIGRAPH_COLOR=${TEAL}
+MINIGRAPH_MINIGRAPH_COLOR=${TEAL}
 GRAPHALIGNER_MINIGRAPH_COLOR=${GREEN}
 GRAPHALIGNER_DEFAULT_COLOR=${YELLOW}
+GRAPHALIGNER_FAST_COLOR=${NEW_COLOR}
 GIRAFFE_COLOR=${DARK_ORANGE}
 GIRAFFE_SAMPLED_COLOR=${ORANGE}
 
@@ -54,16 +56,30 @@ MINIGRAPH_MINIGRAPH=minigraph,hprc-v2.0-minigraph-eval
 GRAPHALIGNER_MINIGRAPH=graphaligner-default,hprc-v2.0-minigraph-eval
 
 GRAPHALIGNER_DEFAULT=graphaligner-default,hprc-v2.0-mc-eval-d46
+GRAPHALIGNER_FAST=graphaligner-fast,hprc-v2.0-mc-eval-d46
 GIRAFFE=giraffe,hprc-v2.0-mc-eval-d46
 
 GIRAFFE_SAMPLED=giraffe,hprc-v2.0-mc-eval.ec1M-sampled16o
+
+###################################### Define conditions and colors for each experiment
+
+HIFI_SIM_1M_HEADLINE_CATEGORIES="${MINIMAP_HIFI} ${WINNOWMAP} ${PBMM} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_MINIGRAPH} ${GRAPHALIGNER_DEFAULT} ${GIRAFFE} ${GIRAFFE_SAMPLED}"
+HIFI_SIM_1M_HEADLINE_COLORS="${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${PBMM_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_MINIGRAPH_COLOR} ${GRAPHALIGNER_MINIGRAPH_COLOR} ${GRAPHALIGNER_DEFAULT_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR}"
+
+R10_SIM_1M_HEADLINE_CATEGORIES="${MINIMAP_R10} ${WINNOWMAP} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_MINIGRAPH} ${GRAPHALIGNER_DEFAULT} ${GIRAFFE} ${GIRAFFE_SAMPLED}"
+R10_SIM_1M_HEADLINE_COLORS="${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_MINIGRAPH_COLOR} ${GRAPHALIGNER_MINIGRAPH_COLOR} ${GRAPHALIGNER_DEFAULT_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR}"
+
+HIFI_REAL_FULL_HEADLINE_CATEGORIES="${MINIMAP_HIFI} ${WINNOWMAP} ${PBMM} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_FAST} ${GRAPHALIGNER_DEFAULT} ${GRAPHALIGNER_FAST} ${GIRAFFE} ${GIRAFFE_SAMPLED}"
+HIFI_REAL_FULL_HEADLINE_COLORS="${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${PBMM_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_MINIGRAPH_COLOR} ${GRAPHALIGNER_FAST_COLOR} ${GRAPHALIGNER_DEFAULT_COLOR} ${GRAPHALIGNER_FAST_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR}"
+R10_REAL_FULL_HEADLINE_CATEGORIES="${MINIMAP_R10} ${WINNOWMAP} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_FAST} ${GRAPHALIGNER_DEFAULT} ${GRAPHALIGNER_FAST} ${GIRAFFE} ${GIRAFFE_SAMPLED}"
+R10_REAL_FULL_HEADLINE_COLORS="${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_MINIGRAPH_COLOR} ${GRAPHALIGNER_FAST_COLOR} ${GRAPHALIGNER_DEFAULT_COLOR} ${GRAPHALIGNER_FAST_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR}"
 
 
 ########################################## ROCs and QQs
 
 # Zoomed in headline roc plots
 HIFI_ACCURACY=${ROOT_DIR}/experiments/hifi_sim_1m_headline/results/compared.tsv
-R10_ACCURACY=${ROOT_DIR}/experiments/r10_sim_1m_headline/results/compared.tsv
+R10_ACCURACY=${ROOT_DIR}/experiments/r10y2025_sim_1m_headline/results/compared.tsv
 
 Rscript plot-roc-hifi.R ${HIFI_ACCURACY} ${OUT_DIR}/roc_hifi_headline_zoomed.pdf
 
@@ -82,53 +98,53 @@ Rscript plot-qq.R ${R10_ACCURACY} ${OUT_DIR}/qq_r10_headline.pdf
 ######################################### Incorrect counts
 
 HIFI_INCORRECT=${ROOT_DIR}/experiments/hifi_sim_1m_headline/results/mapping_stats_sim.tsv
-R10_INCORRECT=${ROOT_DIR}/experiments/r10_sim_1m_headline/results/mapping_stats_sim.tsv
+R10_INCORRECT=${ROOT_DIR}/experiments/r10y2025_sim_1m_headline/results/mapping_stats_sim.tsv
 
-python3 barchart.py <(tail -n +2 ${HIFI_INCORRECT} | cut -f1,5) --divisions <(tail -n +2 ${HIFI_INCORRECT} | cut -f1,4)  --title 'HiFi incorrect read count' --y_label 'Count' --x_label 'Condition' --x_sideways --no_n --categories ${MINIMAP_HIFI} ${WINNOWMAP} ${PBMM} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_DEFAULT} ${GIRAFFE} ${GIRAFFE_SAMPLED}  --colors ${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${PBMM_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_COLOR} ${GRAPHALIGNER_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR} --save ${OUT_DIR}/incorrectness_hifi.pdf
+python3 barchart.py <(tail -n +2 ${HIFI_INCORRECT} | cut -f1,5) --divisions <(tail -n +2 ${HIFI_INCORRECT} | cut -f1,4)  --title 'HiFi incorrect read count' --y_label 'Count' --x_label 'Condition' --x_sideways --no_n --categories ${HIFI_SIM_1M_HEADLINE_CATEGORIES}  --colors ${HIFI_SIM_1M_HEADLINE_COLORS} --save ${OUT_DIR}/incorrectness_hifi.pdf
 
-python3 barchart.py <(tail -n +2 ${R10_INCORRECT} | cut -f1,5) --divisions <(tail -n +2 ${R10_INCORRECT} | cut -f1,4)  --title 'R10 incorrect read count' --y_label 'Count' --x_label 'Condition' --x_sideways --no_n --categories ${MINIMAP_R10} ${WINNOWMAP} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_DEFAULT} ${GIRAFFE} ${GIRAFFE_SAMPLED}  --colors ${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_COLOR} ${GRAPHALIGNER_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR}  --save ${OUT_DIR}/incorrectness_r10.pdf
+python3 barchart.py <(tail -n +2 ${R10_INCORRECT} | cut -f1,5) --divisions <(tail -n +2 ${R10_INCORRECT} | cut -f1,4)  --title 'R10 incorrect read count' --y_label 'Count' --x_label 'Condition' --x_sideways --no_n --categories ${R10_SIM_1M_HEADLINE_CATEGORIES}  --colors ${R10_SIM_1M_HEADLINE_COLORS}  --save ${OUT_DIR}/incorrectness_r10.pdf
 
 ########################################## Softclips
 
 HIFI_SOFTCLIPS=${ROOT_DIR}/experiments/hifi_real_full_headline/results/softclipped_or_unmapped_percent.tsv
-R10_SOFTCLIPS=${ROOT_DIR}/experiments/r10_real_full_headline/results/softclipped_or_unmapped_percent.tsv
+R10_SOFTCLIPS=${ROOT_DIR}/experiments/r10y2025_real_full_headline/results/softclipped_or_unmapped_percent.tsv
 
 #hifi softclips low
 LIMIT=$(python3 get_outlier_limit.py ${HIFI_SOFTCLIPS} small)
-python3 barchart.py ${HIFI_SOFTCLIPS} --max ${LIMIT}  --title 'HiFi Softclipped or Unmapped Bases' --y_label 'Percent of bases' --x_label 'Mapper' --x_sideways --no_n --categories ${MINIMAP_HIFI} ${WINNOWMAP} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_DEFAULT} ${GIRAFFE} ${GIRAFFE_SAMPLED}  --colors ${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_COLOR} ${GRAPHALIGNER_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR} --save ${OUT_DIR}/softclips_hifi_headline_low.pdf
+python3 barchart.py ${HIFI_SOFTCLIPS} --max ${LIMIT}  --title 'HiFi Softclipped or Unmapped Bases' --y_label 'Percent of bases' --x_label 'Mapper' --x_sideways --no_n --categories ${HIFI_REAL_FULL_HEADLINE_CATEGORIES}  --colors ${HIFI_REAL_FULL_HEADLINE_COLORS} --save ${OUT_DIR}/softclips_hifi_headline_low.pdf
 
 #hifi softclips high
 LIMIT=$(python3 get_outlier_limit.py ${HIFI_SOFTCLIPS} big)
-python3 barchart.py ${HIFI_SOFTCLIPS} --min ${LIMIT}  --title 'HiFi Softclipped or Unmapped Bases' --y_label 'Percent of bases' --x_label 'Mapper' --x_sideways --no_n --categories ${MINIMAP_HIFI} ${WINNOWMAP} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_DEFAULT} ${GIRAFFE} ${GIRAFFE_SAMPLED}  --colors ${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_COLOR} ${GRAPHALIGNER_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR} --save ${OUT_DIR}/softclips_hifi_headline_high.pdf
+python3 barchart.py ${HIFI_SOFTCLIPS} --min ${LIMIT}  --title 'HiFi Softclipped or Unmapped Bases' --y_label 'Percent of bases' --x_label 'Mapper' --x_sideways --no_n --categories ${HIFI_REAL_FULL_HEADLINE_CATEGORIES}  --colors ${HIFI_REAL_FULL_HEADLINE_COLORS} --save ${OUT_DIR}/softclips_hifi_headline_high.pdf
 
 #r10 softclips
-python3 barchart.py ${R10_SOFTCLIPS} --title 'R10 Softclipped or Unmapped Bases' --y_label 'Percent of bases' --x_label 'Mapper' --x_sideways --no_n --categories ${MINIMAP_R10} ${WINNOWMAP} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_DEFAULT} ${GIRAFFE} ${GIRAFFE_SAMPLED}  --colors ${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_COLOR} ${GRAPHALIGNER_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR} --save ${OUT_DIR}/softclips_r10_headline_high.pdf
+python3 barchart.py ${R10_SOFTCLIPS} --title 'R10 Softclipped or Unmapped Bases' --y_label 'Percent of bases' --x_label 'Mapper' --x_sideways --no_n --categories ${R10_REAL_FULL_HEADLINE_CATEGORIES}  --colors ${R10_REAL_FULL_HEADLINE_COLORS} --save ${OUT_DIR}/softclips_r10_headline_high.pdf
 
 
 ############################################ Runtime
 
 HIFI_RUNTIME=${ROOT_DIR}/experiments/hifi_real_full_headline/results/run_and_sampling_time_from_benchmark.tsv
-R10_RUNTIME=${ROOT_DIR}/experiments/r10_real_full_headline/results/run_and_sampling_time_from_benchmark.tsv
+R10_RUNTIME=${ROOT_DIR}/experiments/r10y2025_real_full_headline/results/run_and_sampling_time_from_benchmark.tsv
 HIFI_INDEX_TIME=${ROOT_DIR}/experiments/hifi_real_full_headline/results/index_load_time.tsv
-R10_INDEX_TIME=${ROOT_DIR}/experiments/r10_real_full_headline/results/index_load_time.tsv
+R10_INDEX_TIME=${ROOT_DIR}/experiments/r10y2025_real_full_headline/results/index_load_time.tsv
 
 # Higher hifi runtimes
 LIMIT=$(python3 get_outlier_limit.py <(awk -v OFS='\t' '{{print $1,$2/60}}' ${HIFI_RUNTIME}) big)
-python3 barchart.py <(awk -v OFS='\t' '{{print $1,$2/60}}' ${HIFI_RUNTIME}) --divisions <(awk -v OFS='\t' '{{print $1,$2/60}}' ${HIFI_INDEX_TIME}) --min ${LIMIT}  --title 'HiFi Runtime' --y_label 'Time (hours)' --x_label 'Mapper' --x_sideways --no_n --categories ${MINIMAP_HIFI} ${WINNOWMAP} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_DEFAULT} ${GIRAFFE} ${GIRAFFE_SAMPLED}  --colors ${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_COLOR} ${GRAPHALIGNER_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR} --save ${OUT_DIR}/runtime_hifi_headline_slow.pdf
+python3 barchart.py <(awk -v OFS='\t' '{{print $1,$2/60}}' ${HIFI_RUNTIME}) --divisions <(awk -v OFS='\t' '{{print $1,$2/60}}' ${HIFI_INDEX_TIME}) --min ${LIMIT}  --title 'HiFi Runtime' --y_label 'Time (hours)' --x_label 'Mapper' --x_sideways --no_n --categories ${HIFI_REAL_FULL_HEADLINE_CATEGORIES}  --colors ${HIFI_REAL_FULL_HEADLINE_COLORS} --save ${OUT_DIR}/runtime_hifi_headline_slow.pdf
 
 # Lower hifi runtimes
 LIMIT=$(python3 get_outlier_limit.py <(awk -v OFS='\t' '{{print $1,$2/60}}' ${HIFI_RUNTIME}) small)
-python3 barchart.py <(awk -v OFS='\t' '{{print $1,$2/60}}' ${HIFI_RUNTIME}) --divisions <(awk -v OFS='\t' '{{print $1,$2/60}}' ${HIFI_INDEX_TIME}) --max ${LIMIT}  --title 'HiFi Runtime' --y_label 'Time (hours)' --x_label 'Mapper' --x_sideways --no_n --categories ${MINIMAP_HIFI} ${WINNOWMAP} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_DEFAULT} ${GIRAFFE} ${GIRAFFE_SAMPLED}  --colors ${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_COLOR} ${GRAPHALIGNER_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR} --save ${OUT_DIR}/runtime_hifi_headline_fast.pdf
+python3 barchart.py <(awk -v OFS='\t' '{{print $1,$2/60}}' ${HIFI_RUNTIME}) --divisions <(awk -v OFS='\t' '{{print $1,$2/60}}' ${HIFI_INDEX_TIME}) --max ${LIMIT}  --title 'HiFi Runtime' --y_label 'Time (hours)' --x_label 'Mapper' --x_sideways --no_n --categories ${HIFI_REAL_FULL_HEADLINE_CATEGORIES}  --colors ${HIFI_REAL_FULL_HEADLINE_COLORS} --save ${OUT_DIR}/runtime_hifi_headline_fast.pdf
 
 # R10 runtimes as one plot
-python3 barchart.py <(awk -v OFS='\t' '{{print $1,$2/60}}' ${R10_RUNTIME}) --divisions <(awk -v OFS='\t' '{{print $1,$2/60}}' ${R10_INDEX_TIME}) --title 'R10 Runtime' --y_label 'Percent of bases' --x_label 'Mapper' --x_sideways --no_n --categories ${MINIMAP_R10} ${WINNOWMAP} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_DEFAULT} ${GIRAFFE} ${GIRAFFE_SAMPLED}  --colors ${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_COLOR} ${GRAPHALIGNER_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR} --save ${OUT_DIR}/runtime_r10_headline.pdf
+python3 barchart.py <(awk -v OFS='\t' '{{print $1,$2/60}}' ${R10_RUNTIME}) --divisions <(awk -v OFS='\t' '{{print $1,$2/60}}' ${R10_INDEX_TIME}) --title 'R10 Runtime' --y_label 'Percent of bases' --x_label 'Mapper' --x_sideways --no_n --categories ${R10_REAL_FULL_HEADLINE_CATEGORIES}  --colors ${R10_REAL_FULL_HEADLINE_COLORS} --save ${OUT_DIR}/runtime_r10_headline.pdf
 
 ###################################### Memory
 
 HIFI_MEMORY=${ROOT_DIR}/experiments/hifi_real_full_headline/results/memory_from_benchmark.tsv
-R10_MEMORY=${ROOT_DIR}/experiments/r10_real_full_headline/results/memory_from_benchmark.tsv
+R10_MEMORY=${ROOT_DIR}/experiments/r10y2025_real_full_headline/results/memory_from_benchmark.tsv
 
-python3 barchart.py ${HIFI_MEMORY} --title 'HiFi Memory From Benchmark' --y_label 'Memory (GB)' --x_label 'Mapper' --x_sideways --no_n --categories ${MINIMAP_HIFI} ${WINNOWMAP} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_DEFAULT} ${GIRAFFE} ${GIRAFFE_SAMPLED}  --colors ${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_COLOR} ${GRAPHALIGNER_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR}  --save ${OUT_DIR}/memory_hifi.pdf
+python3 barchart.py ${HIFI_MEMORY} --title 'HiFi Memory From Benchmark' --y_label 'Memory (GB)' --x_label 'Mapper' --x_sideways --no_n --categories ${HIFI_REAL_FULL_HEADLINE_CATEGORIES}  --colors ${HIFI_REAL_FULL_HEADLINE_COLORS} --save ${OUT_DIR}/memory_hifi.pdf
 
-python3 barchart.py ${R10_MEMORY} --title 'R10 Memory From Benchmark' --y_label 'Memory (GB)' --x_label 'Mapper' --x_sideways --no_n --categories ${MINIMAP_R10} ${WINNOWMAP} ${GIRAFFE_PRIMARY} ${MINIGRAPH_MINIGRAPH} ${GRAPHALIGNER_DEFAULT} ${GIRAFFE} ${GIRAFFE_SAMPLED}  --colors ${MINIMAP_COLOR} ${WINNOWMAP_COLOR} ${GIRAFFE_PRIMARY_COLOR} ${MINIGRAPH_COLOR} ${GRAPHALIGNER_COLOR} ${GIRAFFE_COLOR} ${GIRAFFE_SAMPLED_COLOR} --save ${OUT_DIR}/memory_r10.pdf
+python3 barchart.py ${R10_MEMORY} --title 'R10 Memory From Benchmark' --y_label 'Memory (GB)' --x_label 'Mapper' --x_sideways --no_n --categories ${R10_REAL_FULL_HEADLINE_CATEGORIES}  --colors ${R10_REAL_FULL_HEADLINE_COLORS} --save ${OUT_DIR}/memory_r10.pdf
 
